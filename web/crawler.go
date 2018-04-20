640c3067b2cf773f99aac7365a78c52b4fa7b147
Samuel Kelemen <Samuel@Kelemen.us>
2018-04-20 13:34:47 -0400

    finish commenting public methods

--- web/crawler.go
+++ web/crawler.go
@@ -4,21 +4,30 @@ import (
 	"github.com/sckelemen/crawler/core"
 )
 
+// WebCrawler is an implementation of ICrawler
+// for crawling websites. It traverses links
+// using dfs
 type WebCrawler struct {
 }
 
+// CrawlerType returns the identifier for the
+// WebCrawler type
 func (wc WebCrawler) CrawlerType() core.CrawlerType {
 	return WebCrawlerType
 }
 
+// Crawl crawls the root string
 func (wc WebCrawler) Crawl(root string) interface{} {
 	return wc
 }
 
 const (
+	// WebCrawlerType is the indentifier for the
+	// WebCrawler implementation
 	WebCrawlerType core.CrawlerType = "web_crawler"
 )
 
+// NewCrawler returns the webcrawlers interface
 func NewCrawler() core.ICrawler {
 	return WebCrawler{}
 }
